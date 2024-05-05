5517c2f815100e9c14ab108494ed968f1de47986
burnedinthesky <andrew.mutien@gmail.com>
2024-05-05 22:13:45 +0800

    Upload sample backend

--- drawing-comp-backend/drawing_comp_backend/main.py
+++ drawing-comp-backend/drawing_comp_backend/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -8,12 +23,18 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+class TeamCode(BaseModel):
+    code: str
 
 
-if __name__ == "__main__":
-    import uvicorn
+@app.post("/api/teams/checkCode")
+async def checkTeamCode(team_code: TeamCode):
+    if team_code.code == "abcde":
+        return {"teamId": "abcder"}
+    raise HTTPException(status_code=404, detail="TEAM_CODE_NOT_FOUND")
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
