e97cd7fd829c55c79ceea15ce2152976a9b932dc
burnedinthesky <andrew.mutien@gmail.com>
2024-05-19 20:43:08 +0800

    Implement Optimistic Updates in Code Submission

--- src/components/challenges/ConfirmUpload.tsx
+++ src/components/challenges/ConfirmUpload.tsx
@@ -7,17 +7,74 @@ import {
     Code,
 } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
+import StatusChip from "./submissions/StatusChip";
 
 export function ConfirmUpload({
     code,
+    challengeId,
     isOpen,
     onOpenChange,
 }: {
     code: string;
+    challengeId: string;
     isOpen: boolean;
     onOpenChange: () => void;
 }) {
+    const queryClient = useQueryClient();
+
+    const submitCodeMutation = useMutation({
+        mutationFn: () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve({});
+                }, 1000);
+            });
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["submissions", challengeId],
+                refetchType: "all",
+            });
+            onOpenChange();
+        },
+        onSuccess: () => {
+            toast.success("提交成功！");
+            queryClient.setQueryData(
+                ["submissions", challengeId],
+                (oldData: any) => {
+                    return [
+                        {
+                            id: uuidv4(),
+                            code: code,
+                            status: <StatusChip status="todo" />,
+                            wordCount: "-",
+                            fitness: "-",
+                            execute_time: "-",
+                            stdout: "",
+                            stderr: "",
+                            line_number: "-",
+                            score: "-",
+                            time: new Date().toLocaleString("en-US", {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                                second: "2-digit",
+                            }),
+                        },
+                        ...oldData,
+                    ];
+                },
+            );
+        },
+        onError: (error) => {
+            toast.error("提交失敗，請再試一次！", {
+                description: error.message ?? error,
+            });
+        },
+    });
+
     return (
         <Modal
             isOpen={isOpen}
@@ -38,9 +95,9 @@ export function ConfirmUpload({
                         <ModalFooter>
                             <Button onPress={onClose}>取消</Button>
                             <Button
+                                isLoading={submitCodeMutation.isPending}
                                 onPress={() => {
-                                    toast.success("程式已提交！");
-                                    onClose();
+                                    submitCodeMutation.mutate();
                                 }}
                                 color="success"
                             >
--- src/components/challenges/ProblemDesc.tsx
+++ src/components/challenges/ProblemDesc.tsx
@@ -1,18 +1,30 @@
 import { BookOpenIcon } from "@heroicons/react/20/solid";
-import { Button, Chip, Image, useDisclosure } from "@nextui-org/react";
+import { Button, Chip, Image, Spinner, useDisclosure } from "@nextui-org/react";
 import { ChallengeType } from "../../types/challenges";
 import { useRef, useState } from "react";
 import { toast } from "sonner";
 import { ConfirmUpload } from "./ConfirmUpload";
+import { useQuery } from "@tanstack/react-query";
+import { SampleChallenge } from "../../utils/fakeData";
 
-export function ProblemDesc({
-    challengeData,
-}: {
-    challengeData: ChallengeType;
-}) {
+interface ProblemDescProps {
+    id: string;
+}
+
+export function ProblemDesc({ id }: ProblemDescProps) {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [userCode, setUserCode] = useState<string>(""); // [1
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    const challengeQuery = useQuery<ChallengeType>({
+        queryKey: ["challenge", id],
+        queryFn: () =>
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(SampleChallenge);
+                }, 1000);
+            }),
+    });
 
     return (
         <div className="w-full pt-4 p-6 rounded-md bg-zinc-800 border border-zinc-400 text-white bg-opacity-70">
@@ -22,62 +34,71 @@ export function ProblemDesc({
                     <h2 className="text-zinc-100 font-medium">題目描述</h2>
                 </div>
             </Chip>
-            <h2 className="text-3xl mt-2 font-medium">
-                請畫一個{challengeData.name}
-            </h2>
-            <div className="flex justify-between mt-2 gap-8">
-                <p className="whitespace-pre-wrap text-left w-7/12 text-zinc-300">
-                    {challengeData.description}
-                </p>
-                <div className="w-5/12">
-                    <Image
-                        alt="Card background"
-                        className="object-cover rounded-xl "
-                        src={challengeData.image}
-                    />
+            {challengeQuery.data ? (
+                <>
+                    <h2 className="text-3xl mt-2 font-medium">
+                        請畫一個{challengeQuery.data.name}
+                    </h2>
+                    <div className="flex justify-between mt-2 gap-8">
+                        <p className="whitespace-pre-wrap text-left w-7/12 text-zinc-300">
+                            {challengeQuery.data.description}
+                        </p>
+                        <div className="w-5/12">
+                            <Image
+                                alt="Card background"
+                                className="object-cover rounded-xl "
+                                src={challengeQuery.data.image_url}
+                            />
+                        </div>
+                    </div>
+                    <hr className="w-full border-b border-b-zinc-600 mt-5 mb-3" />
+                    <div className="mt-2 flex items-center w-full gap-2">
+                        <Button>下載模板</Button>
+                        <Button
+                            onClick={() => {
+                                if (!fileInputRef.current) return;
+                                fileInputRef.current.click();
+                            }}
+                        >
+                            上傳程式
+                        </Button>
+                        <ConfirmUpload
+                            code={userCode}
+                            challengeId={id}
+                            isOpen={isOpen}
+                            onOpenChange={onOpenChange}
+                        />
+                        <input
+                            ref={fileInputRef}
+                            type="file"
+                            accept=".py"
+                            hidden
+                            onChange={(e) => {
+                                if (!e.target.files) return;
+
+                                const file = e.target.files[0];
+
+                                const reader = new FileReader();
+                                reader.readAsText(file, "UTF-8");
+
+                                reader.onload = (readerEvent) => {
+                                    if (!readerEvent.target) {
+                                        toast.error("無法讀取檔案");
+                                        return;
+                                    }
+                                    const content = readerEvent.target.result;
+                                    setUserCode(content as string);
+                                    onOpen();
+                                };
+                            }}
+                        />
+                    </div>
+                </>
+            ) : (
+                <div className="h-64 flex items-center justify-center">
+                    <Spinner label="載入中" />
                 </div>
-            </div>
-            <hr className="w-full border-b border-b-zinc-600 mt-5 mb-3" />
-            <div className="mt-2 flex items-center w-full gap-2">
-                <Button>下載模板</Button>
-                <Button
-                    onClick={() => {
-                        if (!fileInputRef.current) return;
-                        fileInputRef.current.click();
-                    }}
-                >
-                    上傳程式
-                </Button>
-                <ConfirmUpload
-                    code={userCode}
-                    isOpen={isOpen}
-                    onOpenChange={onOpenChange}
-                />
-                <input
-                    ref={fileInputRef}
-                    type="file"
-                    accept=".py"
-                    hidden
-                    onChange={(e) => {
-                        if (!e.target.files) return;
-
-                        let file = e.target.files[0];
-
-                        var reader = new FileReader();
-                        reader.readAsText(file, "UTF-8");
-
-                        reader.onload = (readerEvent) => {
-                            if (!readerEvent.target) {
-                                toast.error("無法讀取檔案");
-                                return;
-                            }
-                            var content = readerEvent.target.result;
-                            setUserCode(content as string);
-                            onOpen();
-                        };
-                    }}
-                />
-            </div>
+            )}
         </div>
     );
 }
