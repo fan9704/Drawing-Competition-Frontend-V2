3ed8eee5c3ac0eae43cba5b6be7036f58e4485a5
fan9704 <cxz123499@gmail.com>
2024-06-25 00:43:30 +0800

    🐳 chore(local ssl): mkcrt

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Unfonts from "unplugin-fonts/vite";
+import path from "path";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        basicSsl(),
+        mkcert(),
         Unfonts({
             custom: {
                 families: [
@@ -17,4 +22,8 @@ export default defineConfig({
             },
         }),
     ],
+    server:{
+        host:"0.0.0.0",
+        https:true
+    },
 });
