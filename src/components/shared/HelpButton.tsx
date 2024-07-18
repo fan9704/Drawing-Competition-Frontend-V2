ee67ddc5cf5b6b2baa438d9892753c1cdc04a083
burnedinthesky <40905726+burnedinthesky@users.noreply.github.com>
2024-07-19 02:04:31 +0800

    Add help button

--- src/components/shared/HelpButton.tsx
+++ src/components/shared/HelpButton.tsx
@@ -1,57 +1,90 @@
-import { useState } from "react";
-import { QuestionMarkCircleIcon } from "@heroicons/react/16/solid";
+import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import {
-    Modal,
-    ModalBody,
-    ModalContent,
-    ModalHeader,
-    Tab,
-    Tabs,
+    Button,
+    Popover,
+    PopoverTrigger,
+    Listbox,
+    ListboxItem,
 } from "@nextui-org/react";
-import TurtleTutorial from "../../content/TurtleTutorial";
-import WebTutorial from "../../content/WebTutorial";
 
 export default function HelpButton() {
-    const [isHelpOpen, setIsHelpOpen] = useState(false);
-
     return (
-        <button
-            className="cursor-pointer"
-            onClick={() => {
-                setIsHelpOpen(true);
-            }}
+        <Popover
+            placement="top-end"
+            className="fixed bottom-10 right-10 hover:shadow-xl z-10"
         >
-            <QuestionMarkCircleIcon className="h-9" />
-            <Modal
-                isOpen={isHelpOpen}
-                onOpenChange={setIsHelpOpen}
-                size="3xl"
-                className="bg-zinc-900"
-                scrollBehavior="inside"
-            >
-                <ModalContent>
-                    {() => (
-                        <>
-                            <ModalHeader className="block">
-                                <h1 className="text-white text-2xl">說明</h1>
-                                <h2 className="text-zinc-400 mt-1 font-medium">
-                                    我們能怎麼幫到你？
-                                </h2>
-                            </ModalHeader>
-                            <ModalBody>
-                                <Tabs aria-label="choose-help" className="dark">
-                                    <Tab key="turtle" title="Turtle">
-                                        <TurtleTutorial />
-                                    </Tab>
-                                    <Tab key="app" title="網頁">
-                                        <WebTutorial />
-                                    </Tab>
-                                </Tabs>
-                            </ModalBody>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
-        </button>
+            <PopoverTrigger>
+                <Button className="fixed bottom-5 right-5 hover:shadow-xl z-10 w-10 h-10 bg-transparent">
+                    <QuestionMarkCircleIcon className="h-9" />
+                </Button>
+            </PopoverTrigger>
+            <div className="w-full max-w-40 border-small border-zinc-700 px-1 py-2 rounded-small bg-zinc-800 fixed bottom-16 right-10">
+                <Listbox
+                    aria-label="Actions"
+                    onAction={(key) => {
+                        const links = {
+                            sys_tut:
+                                "https://docs.google.com/document/d/1Whgj8O9oFgrYUtGFhotWkBH_dA5CbMAGquSgOSCL9Fs/edit",
+                            turtle_slide:
+                                "https://docs.google.com/presentation/d/1_b6YuFZVujgGzW1fH6HVNZluly_iH5Xdxeuc6JTp0F4/edit",
+                            ex_code_1:
+                                "https://github.com/aa35037123/turtle_challenge/blob/main/drawing_tutorial/pen.py",
+                            ex_code_2:
+                                "https://github.com/aa35037123/turtle_challenge/blob/main/drawing_tutorial/star.py",
+                        };
+
+                        const new_window = window.open(
+                            links[
+                                key as
+                                    | "sys_tut"
+                                    | "turtle_slide"
+                                    | "ex_code_1"
+                                    | "ex_code_2"
+                            ],
+                            "_blank",
+                        );
+                        if (new_window) window.focus();
+                    }}
+                >
+                    <ListboxItem
+                        className="text-white"
+                        key="sys_tut"
+                        endContent={
+                            <ArrowTopRightOnSquareIcon className="w-4" />
+                        }
+                    >
+                        系統使用指南
+                    </ListboxItem>
+                    <ListboxItem
+                        className="text-white"
+                        key="turtle_slide"
+                        endContent={
+                            <ArrowTopRightOnSquareIcon className="w-4" />
+                        }
+                    >
+                        Turtle 教學
+                    </ListboxItem>
+                    <ListboxItem
+                        className="text-white"
+                        key="ex_code_1"
+                        endContent={
+                            <ArrowTopRightOnSquareIcon className="w-4" />
+                        }
+                    >
+                        範例程式一
+                    </ListboxItem>
+                    <ListboxItem
+                        className="text-white"
+                        key="ex_code_2"
+                        endContent={
+                            <ArrowTopRightOnSquareIcon className="w-4" />
+                        }
+                    >
+                        範例程式二
+                    </ListboxItem>
+                </Listbox>
+            </div>
+        </Popover>
     );
 }
